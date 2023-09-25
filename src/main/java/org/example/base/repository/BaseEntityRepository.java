0a8c71bc8d28692718c229f4b4ce63318526f085
sahar_gholami <sahar_gholami03@gmail.com>
2023-09-25 14:09:57 +0330

    create crud method in baseRepository interface

--- src/main/java/org/example/base/repository/BaseEntityRepository.java
+++ src/main/java/org/example/base/repository/BaseEntityRepository.java
@@ -1,4 +1,23 @@
 package org.example.base.repository;
 
+import org.example.base.domain.BaseEntity;
+
+import java.sql.SQLException;
+
 public interface BaseEntityRepository {
+
+    BaseEntity[] findAll() throws SQLException;
+
+    BaseEntity findById(Long id) throws SQLException;
+
+    void deleteById(Long id) throws SQLException;
+
+    long count() throws SQLException;
+
+    BaseEntity save(BaseEntity entity) throws SQLException;
+
+    BaseEntity update(BaseEntity entity) throws SQLException;
+
+    boolean existsById(Long id) throws SQLException;
+
 }
