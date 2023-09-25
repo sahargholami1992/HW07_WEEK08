5f53735694e98dc34c8ce79188721216ce31da4f
sahar_gholami <sahar_gholami03@gmail.com>
2023-09-25 14:24:34 +0330

    implement of base entity repository

--- src/main/java/org/example/base/repository/impl/BaseEntityRepositoryImpl.java
+++ src/main/java/org/example/base/repository/impl/BaseEntityRepositoryImpl.java
@@ -1,5 +1,45 @@
 package org.example.base.repository.impl;
 
-public abstract class BaseEntityRepositoryImpl {
+import org.example.base.domain.BaseEntity;
+import org.example.base.repository.BaseEntityRepository;
 
+import java.sql.SQLException;
+
+public class BaseEntityRepositoryImpl implements BaseEntityRepository {
+
+
+    @Override
+    public BaseEntity[] findAll() throws SQLException {
+        return new BaseEntity[0];
+    }
+
+    @Override
+    public BaseEntity findById(Long id) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public void deleteById(Long id) throws SQLException {
+
+    }
+
+    @Override
+    public long count() throws SQLException {
+        return 0;
+    }
+
+    @Override
+    public BaseEntity save(BaseEntity entity) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public BaseEntity update(BaseEntity entity) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public boolean existsById(Long id) throws SQLException {
+        return false;
+    }
 }
