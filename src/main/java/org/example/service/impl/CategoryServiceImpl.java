133e3669df3fdfbaf857182863b639db76d1b5d1
sahar_gholami <sahar_gholami03@gmail.com>
2023-09-28 03:00:28 +0330

    add repository and service to category

---
+++ src/main/java/org/example/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,21 @@
+package org.example.service.impl;
+
+import org.example.base.repository.BaseEntityRepository;
+import org.example.base.service.impl.BaseEntityServiceImpl;
+import org.example.domain.Category;
+import org.example.service.CategoryService;
+
+import java.sql.SQLException;
+
+
+public class CategoryServiceImpl extends BaseEntityServiceImpl
+        implements CategoryService {
+    public CategoryServiceImpl(BaseEntityRepository baseRepository) {
+        super(baseRepository);
+    }
+
+    @Override
+    public Category[] getCategory() throws SQLException {
+        return (Category[]) super.findAll();
+    }
+}
