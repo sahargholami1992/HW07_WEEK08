75b151f4969644d25e6377c69a1539b68de9046a
sahar_gholami <sahar_gholami03@gmail.com>
2023-09-28 02:54:50 +0330

    add base entity service

--- src/main/java/org/example/base/service/BaseEntityService.java
+++ src/main/java/org/example/base/service/BaseEntityService.java
@@ -1,4 +1,21 @@
 package org.example.base.service;
 
+import org.example.base.domain.BaseEntity;
+
+import java.sql.SQLException;
+
 public interface BaseEntityService {
+    BaseEntity[] findAll() throws SQLException;
+
+    BaseEntity findById(Long id) throws SQLException;
+
+    void deleteById(Long id) throws SQLException;
+
+    long count() throws SQLException;
+
+    BaseEntity register(BaseEntity entity) throws SQLException;
+
+    BaseEntity update(BaseEntity entity) throws SQLException;
+
+    boolean existsById(Long id) throws SQLException;
 }
